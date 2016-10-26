fe8e6861f1ea9b2bd2c97a12baf7e2f96ed8e108
juicypasta <jackson.darrow@gmail.com>
2016-10-26 19:48:23 -0400

    init listener setup

---
+++ sniffer.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <pcap.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "packets.h"
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+
+int main(int argc, char **argv) {
+    pcap_t *handle;         /* Session handle */
+    char *dev;          /* The device to sniff on */
+    char errbuf[PCAP_ERRBUF_SIZE];  /* Error string */
+    struct bpf_program fp;      /* The compiled filter */
+    char filter_exp[] = "tcp";  /* The filter expression */
+    bpf_u_int32 mask;       /* Our netmask */
+    bpf_u_int32 net;        /* Our IP */
+    struct pcap_pkthdr header;  /* The header that pcap gives us */
+    const u_char *packet;       /* The actual packet */
+
+    /* Define the device */
+    dev = pcap_lookupdev(errbuf);
+    if (dev == NULL) {
+        fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+        return(2);
+    }
+    /* Find the properties for the device */
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+        net = 0;
+        mask = 0;
+    }
+    /* Open the session in promiscuous mode */
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        return(2);
+    }
+    /* Compile and apply the filter */
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return(2);
+    }
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return(2);
+    }
+
+    int ret = pcap_loop(handle, 1000, got_packet, NULL);
+    printf("%d", ret);
+
+    pcap_close(handle);
+
+    return 0;
+}
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+    // header->ts timestamp
+    // header->caplen len of packet present
+    // header->len len of total packet
+
+    // packet points to the entire packet, this callback triggers whenever we get a header
+
+
+    const struct sniff_ethernet *ethernet; /* The ethernet header */
+    const struct sniff_ip *ip; /* The IP header */
+    const struct sniff_tcp *tcp; /* The TCP header */
+    const char *payload; /* Packet payload */
+
+    u_int size_ip;
+    u_int size_tcp;
+
+    ethernet = (struct sniff_ethernet*)(packet);
+    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20) {
+        printf("   * Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    }
+    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if (size_tcp < 20) {
+        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
+    payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+
+    printf("incoming-----------\n");
+    printf("timestamp: %d\n", header->ts);
+    printf("src ip: %s\n", inet_ntoa(ip->ip_src));
+    printf("dst ip: %s\n", inet_ntoa(ip->ip_dst));
+    printf("ttl: %u\n", ip->ip_ttl);
+
+    printf("src port: %u\n", tcp->th_sport);
+    printf("dst port: %u\n", tcp->th_dport);
+    printf("sequence: %u\n", tcp->th_seq);
+    printf("urgent: %u\n", tcp->th_urp);
+
+}
